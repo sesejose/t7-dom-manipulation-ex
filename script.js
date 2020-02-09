7b8eaab14dfe9c6f5df52ab13eecfab6b998b46e
Jonas Holbech <jofh@kea.dk>
2020-02-09 22:16:19 +0100

    add transition on changes

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //Write your own code here
 //DON'T touch any other files!
+document.querySelectorAll("br").forEach(b => b.remove());
+document.querySelectorAll("section").forEach(s => {
+  s.classList.add("filled");
+  s.previousElementSibling.remove();
+});
