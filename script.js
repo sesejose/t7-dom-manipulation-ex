77836785a3145abcb4b1ac076a135f258dcc604a
Jonas Holbech <jofh@kea.dk>
2020-02-10 22:30:26 +0100

    clean up

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
 //Write your own code here
 //DON'T touch any other files!
-document.querySelectorAll("br").forEach(b => b.remove());
-document.querySelectorAll("section").forEach(s => {
-  s.classList.add("filled");
-  s.previousElementSibling.remove();
-});
